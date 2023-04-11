d8eed2a7becd7ef11204ff7ee31d5b7451bd0272
AlteriusOmega <TheRealSamChaney@gmail.com>
2023-04-10 21:22:30 -0500

    Created Polygon and GridIsometric classes

--- geometry_generator.py
+++ geometry_generator.py
@@ -1,5 +1,6 @@
 import svgwrite
 import math
+from typing import List
 
 # This will be a SVG geometric pattern creator app
 
@@ -11,7 +12,7 @@ def regular_polygon(center, radius, num_points):
     angle = (math.pi * 2) / num_points
     points = []
     for i in range(num_points):
-        current_angle = angle * i + (90*math.pi/180)
+        current_angle = angle * i - (90*math.pi/180)
         x = center[0] + radius * math.cos(current_angle)
         y = center[1] + radius * math.sin(current_angle)
         # print(f" current_angle is {current_angle * (180/math.pi)},x is {x}, y is {y} ")
@@ -83,7 +84,7 @@ def sine_gradient(hex_grid, max_difference, num_waves):
 def modify_grid(grid, callback):
     for i, row in enumerate(grid):
         for j, polygon in enumerate(row):
-            print(f"polygon is {polygon}")
+            # print(f"polygon is {polygon}")
             num_points = len(polygon)
             if num_points % 2 == 0: # Even number of sides
                 middle = int(num_points/2)
@@ -95,35 +96,221 @@ def modify_grid(grid, callback):
                 center = [polygon[0] - edge_center]
             callback(polygon, center, i, j)
 
-class polygon:
-    def __init__(self, drawing, center, radius, num_points):
+class Polygon:
+    def __init__(self, drawing, center, radius, num_points, angle=0):
         self.drawing = drawing
-        self.center = center
-        self.radius = radius
-        self.num_points = num_points
-        self.points = regular_polygon(center, radius, num_points)
-        
+        self._center = center
+        self._radius = radius
+        self._num_points = num_points
+        self._angle = angle
+        self.points = self.polygon()
     
-    def draw(self):
-        drawing.add(drawing.polygon())
+    def polygon(self):
+        polygon_angle = (math.pi * 2) / self._num_points
+        points = []
+        for i in range(self._num_points):
+            current_angle = polygon_angle * i - (90*math.pi/180) + self._angle*(math.pi/180)
+            # print(f"self._center is {self._center}")
+            x = self._center[0] + self._radius * math.cos(current_angle)
+            y = self._center[1] + self._radius * math.sin(current_angle)
+            # print(f" current_angle is {current_angle * (180/math.pi)},x is {x}, y is {y} ")
+            points.append([x, y])
+        return points
         
-
+    def draw(self):
+        drawing.add(drawing.polygon(self.points))
+        
+    def rotate(self, rotation_angle:float):
+        self._angle += rotation_angle
+        self.points = self.polygon()
+        
+    @property
+    def center(self):
+        return self._center
+    
+    @center.setter
+    def center(self, center: List[float]):
+        if len(center) == 2:
+            self._center = center
+            self.points = self.polygon()
+        else:
+            print(f"Center must be list of length 2, defining x and y coordinates")
+            
+    @property
+    def radius(self):
+        return self._radius
+    
+    @radius.setter
+    def radius(self, radius:float):
+        self._radius = radius
+        self.points = self.polygon()
+        
+    @property
+    def num_points(self):
+        return self._num_points
+    
+    @num_points.setter
+    def num_points(self, num_points:int):
+        self._num_points = num_points
+        self.points = self.polygon()
+        
+    @property
+    def angle(self):
+        return self._angle
+    
+    @angle.setter
+    def angle(self, angle:float):
+        self._angle = angle
+        self.points = self.polygon()
+        
+class GridIsometric:
+    def __init__(self, drawing, spacing, num_x, num_y, radius, num_points, angle=0):
+        self.drawing = drawing
+        self._spacing = spacing
+        self._num_x = num_x
+        self._num_y = num_y
+        self._radius = radius
+        self._num_points = num_points
+        self._angle = angle
+        self.points = self.grid()
+        self.polygons = self.polygons()
+        # print(f"In init, self.points is {self.points}, self.polygons is {self.polygons}")   
+        
+    def grid(self):
+        spacing_y = self._spacing * math.sin(60*(math.pi/180))
+        grid = []
+        for i in range(self._num_y):
+            row = []
+            for j in range(self._num_x):
+                if i % 2 == 0:
+                    x = j * self._spacing
+                else:
+                    x = j * self._spacing + (self._spacing/2)
+                y = i * spacing_y
+                row.append([x, y])
+            grid.append(row)
+        # print(grid)
+        return grid
+    
+    def polygons(self):
+        polygons = []
+        for i, row in enumerate(self.points):
+            polygon_row = []
+            for j, coord in enumerate(row):
+                polygon = Polygon(self.drawing, coord, self._radius, self._num_points, self._angle)
+                polygon_row.append(polygon)
+            polygons.append(polygon_row)
+        return polygons
+    
+    def modify_polygons(self, callback):
+        polygons = []
+        for i, row in enumerate(self.polygons):
+            for j, polygon in enumerate(row):
+                # print(f"in modify_polygons and polygon is {polygon}")
+                callback(polygon, i, j, self.num_x, self.num_y)
+                  
+    def draw_polygons(self):
+        # print(f"draw_polygons was called")
+        self.modify_polygons(lambda polygon, i, j, num_x, num_y: self.drawing.add(self.drawing.polygon(polygon.points)))
+        
+    @property
+    def spacing(self):
+        return self._spacing
+    
+    @spacing.setter
+    def spacing(self, spacing:float):
+        self._spacing = spacing
+        self.points = self.grid()
+    
+    @property
+    def num_x(self):
+        return self._num_x
+    
+    @num_x.setter
+    def spacing(self, num_x:int):
+        self._num_x = num_x
+        self.points = self.grid()
+    
+    @property
+    def num_y(self):
+        return self._num_y
+    
+    @num_y.setter
+    def spacing(self, num_y:int):
+        self._num_y = num_y
+        self.points = self.grid()
+        
+    @property
+    def radius(self):
+        return self._radius
+    
+    @radius.setter
+    def radius(self, radius:float):
+        self._radius = radius
+        self.points = self.polygons()
+        
+    @property
+    def num_points(self):
+        return self._num_points
+    
+    @num_points.setter
+    def num_points(self, num_points:int):
+        self._num_points = num_points
+        self.points = self.polygons()
+        
+    @property
+    def angle(self):
+        return self._angle
+    
+    @angle.setter
+    def angle(self, angle:float):
+        self._angle = angle
+        self.points = self.polygons()
+        
 def print_polygon_center(polygon, center, i, j):
     print(center)
     
-test_polygon = polygon(drawing, [0,0], 5, 6)
-test_polygon.num_points = 5
-print(test_polygon.num_points)
-print(test_polygon.points)
+    
+def radius_morph_1(polygon:Polygon, i:int, j:int, num_x:int, num_y:int):
+    center_x = (num_x - 1)/2
+    center_y = (num_y - 1)/2
+    difference_x = abs(center_x - j)
+    difference_y = abs(center_y - i)
+    # print(f"i is {i}, j is {j}, center_x is {center_x}, difference_x is {difference_x}")
+    polygon.radius += (difference_x + difference_y)/10
+    # polygon.rotate(i+j*5)
 
-hex_grid_1 = hex_grid(2, 5, 20, 100)
+def radius_morph_2(polygon:Polygon, i:int, j:int, num_x:int, num_y:int):
+    center_x = (num_x - 1)/2
+    center_y = (num_y - 1)/2
+    difference_x = abs(center_x - j)
+    difference_y = abs(center_y - i)
+    # print(f"i is {i}, j is {j}, center_x is {center_x}, difference_x is {difference_x}")
+    polygon.radius -= (difference_x + difference_y)/10
+    
+test_polygon = Polygon(drawing, [0,0], 5, 6)
+# test_polygon._num_points = 5
+test_polygon.center = [0.0, 0]
+# print(test_polygon._num_points)
+# print(test_polygon.points)
 
-sine_gradient(hex_grid_1, 1, 1)
+test_polygon.draw()
 
-draw_grid(hex_grid_1, drawing)    
+test_polygon.num_points = 3
+test_polygon.center = [50, -20]
+test_polygon.rotate(10)
+test_polygon.draw()
 
-test_hex_grid = hex_grid(10, 20, 1, 1)
+test_grid = GridIsometric(drawing, 10, 21, 51, 2, 6)
+test_grid.modify_polygons(radius_morph_1)
+test_grid.draw_polygons()
+test_grid.modify_polygons(radius_morph_2)
+test_grid.draw_polygons()
 
-modify_grid(test_hex_grid, print_polygon_center)
+# hex_grid_1 = hex_grid(2, 5, 20, 100)
+# sine_gradient(hex_grid_1, 1, 1)
+# draw_grid(hex_grid_1, drawing)    
+# test_hex_grid = hex_grid(10, 20, 1, 1)
+# modify_grid(test_hex_grid, print_polygon_center)
 
 drawing.save()
